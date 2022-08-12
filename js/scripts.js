1b8d8bc88935250ee8590cd2a05a4e1020c98e86
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-12 10:10:37 -0700

    debug branch structure

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,24 @@ window.onload = function() {
   event.preventDefault();
   let c = document.getElementById("c");
   c.setAttribute("class", "hidden");
-  let python = document.getElementById("Python");
+  let python = document.getElementById("python");
   python.setAttribute("class", "hidden");
-
-  
+  let javascript = document.getElementById("javascript");
+  javascript.setAttribute("class", "hidden")
   // getting user input value
   const answer1 = document.getElementById("input1").value.toLowerCase();
-
-
-
+  const answer2 = document.getElementById("input2").value.toLowerCase();
+  const answer3 = document.getElementById("input3").value.toLowerCase();
+  const answer4 = document.getElementById("input4").value.toLowerCase();
+  const answer5 = document.getElementById("input5").value.toLowerCase();
 
 
   if (answer1 === "c") {
     c.removeAttribute("class");
-  } else if  (answer1 === "Python") {
+  } else if  (answer1 === "python") {
     python.removeAttribute("class");
-  } else if (answer1 === "Javascript") {
-    javaScript.removeAttribute("class")
+  } else if (answer1 === "javascript") {
+    javascript.removeAttribute("class")
   }
   }
 }
