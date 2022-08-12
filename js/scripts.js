6f046db75f9e82b30e4e854fa6506a7ad7942614
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-12 11:17:57 -0700

    WIP change input type to multiple choice

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
+//first question
 window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = function(event) {
   event.preventDefault();
-  let c = document.getElementById("c");
+  let c = document.getElementById("c#");
   c.setAttribute("class", "hidden");
   let python = document.getElementById("python");
   python.setAttribute("class", "hidden");
@@ -10,13 +11,11 @@ window.onload = function() {
   javascript.setAttribute("class", "hidden")
   // getting user input value
   const answer1 = document.getElementById("input1").value.toLowerCase();
-  const answer2 = document.getElementById("input2").value.toLowerCase();
-  const answer3 = document.getElementById("input3").value.toLowerCase();
-  const answer4 = document.getElementById("input4").value.toLowerCase();
-  const answer5 = document.getElementById("input5").value.toLowerCase();
+ 
 
 
-  if (answer1 === "c") {
+//first question
+  if (answer1 === "c#") {
     c.removeAttribute("class");
   } else if  (answer1 === "python") {
     python.removeAttribute("class");
@@ -25,3 +24,4 @@ window.onload = function() {
   }
   }
 }
+
