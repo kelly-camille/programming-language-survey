8669a5d701288cb4bcc722423b09104669428f41
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-12 15:37:54 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Programming Language Survey
+
+#### Kelly Bruce
+
+#### This application askes five questions to dermine which programming language is best for them 
+
+## Technologies Used
+
+* CSS
+* HTML
+* JavaScript
+
+## Description
+
+This application is designed to output the programming language that the user prefers
+
+## Setup/Installation Requirements
+
+* Open Terminal 
+* git clone https://github.com/kelly-camille/programming-language-survey.git
+* cd Programming-Language-Survey
+* code .
+
+
+## Known Bugs
+Answer 2-5 variables are defined but never read. 
+
+
+## License
+
+MIT
+
+Copyright (c) 8/12/2022 Kelly Bruce
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
-//first question
 window.onload = function() {
   const form = document.querySelector("form");
   form.onsubmit = function(event) {
-  event.preventDefault();
-  let c = document.getElementById("c#");
-  c.setAttribute("class", "hidden");
-  let python = document.getElementById("python");
-  python.setAttribute("class", "hidden");
-  let javascript = document.getElementById("javascript");
-  javascript.setAttribute("class", "hidden")
-  // getting user input value
-  const answer1 = document.getElementById("input1").value.toLowerCase();
-  const answer2 = document.querySelector("input[name='question#2']:checked").value;
-  const answer3 = document.querySelector("input[name='question#3']").value;
-  const answer4 = document.getElementById("input4").value.toLowerCase();
-  const answer5 = document.getElementById("input5").value.toLowerCase();
-//first question
-  if (answer1 === "c#") {
-    c.removeAttribute("class");
-  } else if  (answer1 === "python") {
-    python.removeAttribute("class");
-  } else if (answer1 === "javascript") {
-    javascript.removeAttribute("class")
-  }
-}
-}
+    event.preventDefault();
+    let c = document.getElementById("c#");
+    c.setAttribute("class", "hidden");
+    let python = document.getElementById("python");
+    python.setAttribute("class", "hidden");
+    let javascript = document.getElementById("javascript");
+    javascript.setAttribute("class", "hidden")
+
+    const answer1 = document.getElementById("input1").value.toLowerCase();
+    const answer2 = document.querySelector("input[name='question#2']:checked").value;
+    const answer3 = document.querySelector("input[name='question#3']").value;
+    const answer4 = document.getElementById("input4").value.toLowerCase();
+    const answer5 = document.getElementById("input5").value.toLowerCase();
+
+    if (answer1 === "c#") {
+      c.removeAttribute("class");
+    } else if  (answer1 === "python") {
+      python.removeAttribute("class");
+    } else if (answer1 === "javascript") {
+      javascript.removeAttribute("class")
+    }
+  };
+};
