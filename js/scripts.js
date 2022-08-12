796f13f47b5b56eb40057fb5596d8dcf3a0d530f
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-12 13:41:11 -0700

    WIP creating boolean statements to configure multiple answers based on user entry

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,10 @@ window.onload = function() {
   javascript.setAttribute("class", "hidden")
   // getting user input value
   const answer1 = document.getElementById("input1").value.toLowerCase();
- 
-
-
+  const answer2 = document.querySelector("input[name='question#2']:checked").value;
+  const answer3 = document.querySelector("input[name='question#3']").value;
+  const answer4 = document.getElementById("input4").value.toLowerCase();
+  const answer5 = document.getElementById("input5").value.toLowerCase();
 //first question
   if (answer1 === "c#") {
     c.removeAttribute("class");
@@ -21,7 +22,13 @@ window.onload = function() {
     python.removeAttribute("class");
   } else if (answer1 === "javascript") {
     javascript.removeAttribute("class")
+  } 
   }
   }
-}
+  
+ 
+  
+  
+
+
 
