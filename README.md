f27d402f6c4ae4a2a4d2d0e7bc51e1186bbeed6b
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-08-20 15:13:45 -0700

    update readme

--- README.md
+++ README.md
@@ -16,10 +16,9 @@ This application is designed to output a programming language selection based on
 
 ## Setup/Installation Requirements
 
-* Open Terminal 
-* git clone https://github.com/kelly-camille/programming-language-survey.git
-* cd Programming-Language-Survey
-* code .
+* Clone using the repo link: https://github.com/kelly-camille/programming-language-survey.git
+* Open file named Programming-Language-Survey
+* open index.html in browser
 * To view: https://kelly-camille.github.io/programming-language-survey/
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-  const form = document.querySelector("form");
-  form.onsubmit = function(event) {
+window.addEventListener("load", function () {
+  const form = document.getElementById("survey");
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
     let c = document.getElementById("c#");
     c.setAttribute("class", "hidden");
@@ -9,24 +9,26 @@ window.onload = function() {
     let javascript = document.getElementById("javascript");
     javascript.setAttribute("class", "hidden")
 
+
     const answer1 = document.getElementById("input1").value.toLowerCase();
     const answer2 = document.querySelector("input[name='question#2']:checked").value;
     const answer3 = document.querySelector("input[name='question#3']:checked").value;
     const answer4 = document.querySelector("input[name='question#4']:checked").value;
     const answer5 = document.querySelector("input[name='question#5']:checked").value;
     console.log("answer2", answer2, "answer3", answer3, "answer4", answer4, "answer5", answer5)
-    
+
     if (answer1 === "c#" && answer2 === "pink" && answer3 === "cat" && answer4 === "country" && answer5 === "sweet") {
       c.removeAttribute("class");
     } else if (answer1 === "c#" && answer2 === "green" && answer3 === "cow" && answer4 === "city" && answer5 === "savory") {
       python.removeAttribute("class");
-    } else if(answer1 === "python" && answer2 === "green" && answer3 === "cat" && answer4 === "city" && answer5 ==="savory") {
+    } else if (answer1 === "python" && answer2 === "green" && answer3 === "cat" && answer4 === "city" && answer5 === "savory") {
       javascript.removeAttribute("class");
     } else {
       python.removeAttribute("class");
-  }
-  };
-};
+    }
+  });
+  form.removeEventListener("submit", function () {
+  });
+});
 
 
- 
